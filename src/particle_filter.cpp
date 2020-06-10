efde13103ae2659e7dacc0588faf4c60f3595a51
Marius-Juston <Marius.juston@hotmail.fr>
2020-06-09 23:52:19 -0400

    reduced the number of particles

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -31,7 +31,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 100;  // TODO: Set the number of particles
+  num_particles = 5;  // TODO: Set the number of particles
 
   // This line creates a normal (Gaussian) distribution for x
   normal_distribution<double> dist_x(x, std[0]);
